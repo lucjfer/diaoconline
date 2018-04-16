465668eceaeb2f77b4a5eced9942bb1c890d92ab
lucjfer <lucjfer0407@gmail.com>
2018-04-16 17:56:27 +0700

    update

--- application/views/layouts/banner.php
+++ application/views/layouts/banner.php
@@ -1,23 +1,30 @@
-<div id="main-height">
-    <div class="magestore-bannerslider-standard">
-        <script src="<?php echo base_url('themes/website/script/flexslider.js')?>"></script>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $('.flexslider-7-1').flexslider({animation: "fade", slideshowSpeed: 4500});
-            });
-        </script>
-        <?php
-            $query = $this->db->query("SELECT * FROM ci_banners WHERE publish = 1 AND type = 'home'");
-            $banners = $query->result('Banner');
-        ?>
-        <?php if (count($banners) > 0): ?>
-            <div class="flexslider flexslider-7-1" style="overflow:hidden">
-                <ul class="slides">
-                    <?php foreach ($banners as $banner): ?>
-                        <li><a href="<?php echo !empty($banner->url) ? $banner->url : 'javascript:void(0)'?>" target='_blank' rel='nofollow'><img border=0 src="<?php echo $banner->get_image() ?>" alt="<?php echo $banner->name ?>"/></a></li>
-                    <?php endforeach ?>
-                </ul>
-            </div><!--flexslider-->
-        <?php endif ?>
+<div id="position_1" class ='banner_960x75 margin_bottom'>
+    <div>
+        <a href="http://adv.diaoconline.vn/DiaOcOnline/ThongBaoThayDoiCuPhapSMS/index.html" target="_blank">
+            <img src="../image.diaoconline.vn/banner-dool/2018/04/04/AAA-960x90.jpg" width="960px" height="90px"/>
+        </a>
+    </div>
+    <div>
+        <a href="http://adv.diaoconline.vn/DiaOcOnline/ThongBaoThayDoiCuPhapSMS/index.html" target="_blank">
+            <img src="../image.diaoconline.vn/banner-dool/2018/04/04/AAA-960x90.jpg" width="960px" height="90px"/>
+        </a>
     </div>
 </div>
+<script type='text/javascript'>
+    var Banner1=1;
+    function Random_Banner1() {
+        var _Arr=document.getElementById("position_1").getElementsByTagName("div");
+        for (i=0; i<=_Arr.length-1; i++) {
+            _Arr[i].className='bannerHide';    
+        }    
+        _Arr[Banner1 - 1].className='bannerShow';    
+        var tempBanner = $(_Arr[Banner1 - 1]).html(); 
+        $(_Arr[Banner1 - 1]).html(''); 
+        $(_Arr[Banner1 - 1]).html(tempBanner);    
+        window.setTimeout("Random_Banner1()" ,25000);    
+        Banner1 = Banner1 + 1;    
+        if(Banner1 > _Arr.length)        
+            Banner1 = 1;
+    }
+    Random_Banner1();
+</script>
--- application/views/layouts/head.php
+++ application/views/layouts/head.php
@@ -1,18 +1,19 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<link rel="icon" href="http://www.diaoconline.vn/favicon.ico" type="image/x-icon">
-<link rel="shortcut icon" href="http://www.diaoconline.vn/favicon.ico" type="image/x-icon">
-<title>Cổng thông tin và giao dịch nhà đất - DiaOcOnline</title>
-<meta name="description" content="Phân phối, Laptop, chính hãng, giá rẻ, laptop hà nôị"/>
-<meta name="keywords" content=""/>
-<meta name="robots" content="INDEX,FOLLOW"/>
-<link rel="icon" href="<?php echo base_url('themes/website/images/favicon.ico')?>" type="image/x-icon"/>
-<link rel="shortcut icon" href="<?php echo base_url('themes/website/images/favicon.ico')?>" type="image/x-icon"/>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-<link href="<?php echo base_url('themes/website/css/reset.css')?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo base_url('themes/website/css/general30c8.css?1419')?>" rel="stylesheet" type="text/css" />
-
-<link href="<?php echo base_url('themes/website/js/slides/slides.css')?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo base_url('themes/website/js/tooltip/tools.css')?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo base_url('themes/website/css/uniform.default.css')?>" rel="stylesheet" type="text/css" />
-<link href="<?php echo base_url('themes/website/css/doolv3.css')?>" rel="stylesheet" type="text/css" />
-<script src="<?php echo base_url('themes/website/js/jquery-1.7.2.min.js')?>" type="text/javascript"></script>
+<!-- Mirrored from diaoconline.vn/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 15 Apr 2018 07:19:38 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+    <link rel="icon" href="http://www.diaoconline.vn/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="http://www.diaoconline.vn/favicon.ico" type="image/x-icon">
+    <title>Cổng thông tin và giao dịch nhà đất - DiaOcOnline</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="<?php echo base_url('themes/website/css/reset.css')?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url('themes/website/css/general30c8.css?1419')?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url('themes/website/js/slides/slides.css')?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url('themes/website/js/tooltip/tools.css')?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url('themes/website/css/uniform.default.css')?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url('themes/website/css/doolv3.css')?>" rel="stylesheet" type="text/css" />
+    <script src="<?php echo base_url('themes/website/js/jquery-1.7.2.min.js')?>" type="text/javascript"></script>
+    <script src="<?php echo base_url('themes/website/js/js.cookie.js')?>" type="text/javascript"></script>
+</head>
